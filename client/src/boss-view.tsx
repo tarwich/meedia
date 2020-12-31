578d6f90fc6e6d3698cb1db7e4879560242ff776
Samuel Dillow <tarwich@gmail.com>
2020-12-31 10:48:24 -0600

    Clear click count after 5 seconds

--- client/src/boss-view.tsx
+++ client/src/boss-view.tsx
@@ -1,33 +1,40 @@
 import faker from 'faker';
 import { capitalize } from 'lodash';
-import { useLocalObservable } from 'mobx-react-lite';
+import { observer, useLocalObservable } from 'mobx-react-lite';
+import { useTimeout } from 'rooks';
 import { VBox } from './ui/vbox';
 
 export type BossViewProps = {
   setBossMode(value: boolean): void;
 };
 
-export const BossView = (props: BossViewProps) => {
+const makeSentence = () => capitalize(faker.hacker.phrase().replace('!', '.'));
+
+export const BossView = observer((props: BossViewProps) => {
   const store = useLocalObservable(() => ({
     clickCount: 0,
+    title: makeSentence(),
+    paragraphs: [...new Array(8)].map((_, iParagraph) =>
+      [...new Array(10)].map(() => makeSentence()).join(' ')
+    ),
   }));
+  const { start, clear } = useTimeout(() => void (store.clickCount = 0), 5000);
 
   return (
     <VBox
       onClick={() => {
+        clear();
+        start();
         ++store.clickCount;
 
         if (store.clickCount >= 5) props.setBossMode(false);
       }}
     >
-      <h4>{faker.hacker.phrase()}</h4>
-      {[...new Array(8)].map((_, iParagraph) => (
-        <p key={iParagraph}>
-          {[...new Array(10)]
-            .map(() => capitalize(faker.hacker.phrase()))
-            .join(' ')}
-        </p>
+      <h4>{store.title}</h4>
+      {store.paragraphs.map((text, iParagraph) => (
+        <p key={iParagraph}>{text}</p>
       ))}
+      <span>{''.padEnd(store.clickCount, '.')}</span>
     </VBox>
   );
-};
+});
